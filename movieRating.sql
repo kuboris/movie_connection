b114d5d404c5b2a42e8495591988933418c81bf8
Jakub Klauco <kuboris@users.noreply.github.com>
2016-11-03 08:08:57 +0100

    Update movieRating.sql

--- movieRating.sql
+++ movieRating.sql
@@ -1,3 +1,6 @@
+select source,target,ranked*1000000000000000000 as weight,derank
+from
+(
 SELECT a.a_movie as source,
 a.b_movie as target,
 f0_,
@@ -5,7 +8,8 @@ b.maxed,
 c.maxed2,
 (f0_/(maxed*maxed)) as weight,
 (f0_/(c.maxed2*c.maxed2)) as weight2,
-(f0_/(maxed*maxed))*(f0_/(c.maxed2*c.maxed2)) as ranked
+(f0_/(maxed*maxed))*(f0_/(c.maxed2*c.maxed2)) as ranked,
+RANK() OVER (PARTITION BY a.a_movie ORDER BY ranked DESC) as derank
 FROM [bookrecomedation:CSFD.movieOutput] as a
 LEFT JOIN (
 SELECT a_movie,sum(f0_) as maxed
@@ -22,7 +26,9 @@ GROUP BY 1
 ) as c
 ON a.b_movie = c.b_movie
 WHERE a.a_movie <> a.b_movie
-AND a.a_movie = 241059
+--AND a.a_movie = 7700
+) as d
+where derank < 20
 --AND f0_ > 1000
-ORDER BY 8 desc
+--ORDER BY 8 desc
 --LIMIT 1000
